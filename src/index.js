64d0f8d109085265dc9b4b9920421d68864f0351
Ralph Shui <rskc.2112@gmail.com>
2023-06-16 10:03:27 -0400

    completing fetch demand with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
+  const form = document.querySelector("form");
+ 
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const input = document.getElementById("searchByID");
+    
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        const title = document.querySelector("#movieDetails h4");
+        const summary = document.querySelector("#movieDetails p");
+
+        if(data.id) {
+          title.textContent = data.title;
+          summary.textContent = data.summary;         
+        } else {
+          console.log("Movie Not Found")
+        }
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
